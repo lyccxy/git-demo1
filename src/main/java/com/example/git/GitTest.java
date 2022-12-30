f05a813b75979a55e72d1f3bbf25465d901fad61
林粤铖 <956458104@qq.com>
2022-12-30 17:48:14 +0800

    push commit

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("lyc test4");
         System.out.println("push test4");
         System.out.println("push test3");
+        System.out.println("push test2");
     }
 }
