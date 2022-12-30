08c72474b7caaf27a791fc6eafda1fb8bb9edde7
林粤铖 <956458104@qq.com>
2022-12-30 17:34:29 +0800

    lyc commit4

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("lyc test");
         System.out.println("lyc test2");
+        System.out.println("lyc test3");
+        System.out.println("lyc test4");
     }
 }
